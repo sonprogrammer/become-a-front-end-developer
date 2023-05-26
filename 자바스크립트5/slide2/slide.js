8e6c43d7a6bda6934422eb88d056c6fef34667c9
손영진 <ods04138@gmail.com>
2023-05-26 22:48:22 +0900

    슬라이드 메뉴 구현중현

---
+++ 자바스크립트5/slide2/slide.js
@@ -0,0 +1,53 @@
+const menuContainer = document.querySelector('.menu')
+const menuControlsContainer = document.querySelector('.menu-controls')
+const menuControls = ['previous', 'next']
+const menuItems = document.querySelectorAll('menu1')
+
+class project{
+    constructor(container, items, controls){
+        this.projectContainer = container
+        this.projectControls = controls
+        this.projectArray = [...items]
+    }
+
+    updateMenu(){
+        this.projectArray.forEach(el =>{
+            el.classList.remove('board')
+            el.classList.remove('Map')
+            el.classList.remove('timetable')
+        })
+        this.projectArray.slice(0, 5).forEach((el, i) =>{
+            el.classList.add(`item-${i+1}`)
+        })
+    }
+
+    setCurrentState(direction){
+        if(direction.className == 'menu-controls-previous'){
+            this.projectArray.unshift(this.projectArray.pop())
+        }else{
+            this.projectArray.push(this.projectArray.shift())
+        }
+        this.updateMenu()
+    }
+    setControls(){
+        this.projectControls.forEach(control =>{
+            menuControlsContainer.appendChild(document.createElement('button')).className = `menu-controls-${control}`
+            document.querySelector(`.menu-controls-${control}`).innerText = control
+        })
+    }
+
+    useControls(){
+        const triggers = [...menuControlsContainer.childNodes]
+        triggers.forEach(control =>{
+            control.addEventListener('click', e=>{
+                e.preventDefault()
+                this.setCurrentState(control)
+            })
+        })
+    }
+}
+
+const exampleproject = new project(menuContainer, menuItems, menuControls)
+
+exampleproject.setControls()
+exampleproject.useControls()
