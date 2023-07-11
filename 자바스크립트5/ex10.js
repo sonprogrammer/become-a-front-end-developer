0d9ca5204fce562a9fbef539e20af74d2487a0fe
손영진 <ods04138@gmail.com>
2023-07-11 18:55:10 +0900

    practice

---
+++ 자바스크립트5/ex10.js
@@ -0,0 +1,65 @@
+// function sol(){
+//     return "\"i'm youngjinson\""
+// }
+
+// console.log(sol())
+
+// function sol(){
+//     return "400\nbus\nlike"
+// }
+
+// function sol(){
+//     let num = 400;
+//     let str = "bus"
+//     let str1 = "like"
+
+//     return num +"\t"+ str+"\t" + str1
+// }
+
+// function sol(){
+//     let result = []
+//     let num1 = 1 
+//     let num2 = 2
+//     let str1 = "1"
+//     let str2 = "2"
+
+//     result.push(num1 + num2)
+//     result.push(str1 + str2)
+//     return result
+// }
+
+// function sol(input){
+//     let result
+//     result = input.split("\n")
+//     return result
+//     console.log("elice,rabbit,cheshire,dood,hatter")
+//     // let arr = "goodagoodabad".split("a")
+//     // console.log(arr)
+//     document.write("elice,rabbit,cheshire,dood,hatter")
+// }
+
+// function sol(input){
+//     let answer = []
+//     // answer = input.split(", ")
+//     let str = input.toString()
+//     let a = str.split(", ")
+//     answer.push(typeof a)
+//     answer.push(a[0])
+//     return answer
+// }
+
+
+
+// function sol(money){
+//     let a = money.split("500원")
+//     return a
+// }
+
+
+// function sol(hangul){
+//     return hangul[0] + hangul[2] + hangul[3]
+// }
+
+fucn
+
+console.log(sol("가나다라마바사"))
