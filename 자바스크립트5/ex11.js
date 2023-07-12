9f6d6a229ce1116d4853dbdb4bc1c95725f30581
손영진 <ods04138@gmail.com>
2023-07-12 17:49:18 +0900

    practice

---
+++ 자바스크립트5/ex11.js
@@ -0,0 +1,40 @@
+// remind 3,6,9game
+// function sol(input){
+//     let result = []
+//     for(let i = 1; i <= input; i++){
+//         if((i % 10) %3 == 0 && i % 10 != 0){
+//             result.push("짝")
+//         }else if((Math.floor(i/10) % 3 == 0 && Math.floor(i/10) != 0)){
+//             result.push("짝")
+//         }
+//  // 짝짝이 나오려면 일의 숫자랑 십의자리 숫자가 3, 6, 9가 있어야함 
+
+//         else{
+//             result.push(i)
+//         }
+//     }
+//     return result
+    
+// }
+
+// remind 약수 찾기
+// function sol(input){
+//     let result = []
+//     for(let i =1; i <=input; i++){
+//         if(input % i == 0){
+//             result.push(i)
+//         }
+//     }
+//     return result
+// }
+
+// 숫자 출력
+// function sol(input){
+//     let answer = []
+//     for(let i = 1; i<=input; i++){
+//         answer.push(i)
+//     }
+//     return answer;
+// }
+
+console.log(sol(3))
