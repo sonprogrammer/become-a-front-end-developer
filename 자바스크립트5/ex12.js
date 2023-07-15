2e6ecb33261ad724283133d6d45575ec8dab99a9
손영진 <ods04138@gmail.com>
2023-07-15 16:36:40 +0900

    practice1

--- 자바스크립트5/ex12.js
+++ 자바스크립트5/ex12.js
@@ -3,8 +3,15 @@
 //     return "Hi, youngjin"
 // }
 
+// 2번
+// function sol(){
+//     return "400\nbus\nlike"
+// }
 
-
+//3번
+function sol(){
+    return "\"I'm youngjin\""
+}
 
 
 console.log(sol())
