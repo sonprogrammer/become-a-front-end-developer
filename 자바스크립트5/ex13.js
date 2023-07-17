6caf8b71b61ddc5f1dbe6fc34573530862d77ac8
손영진 <ods04138@gmail.com>
2023-07-17 22:20:16 +0900

    practice3

--- 자바스크립트5/ex13.js
+++ 자바스크립트5/ex13.js
@@ -47,18 +47,62 @@
 
 
 // 13번 - 3, 6, 9게임
-function sol(input){
-    let answer = []
-    for(let i = 1; i <= input; i++){
-        if((i % 10) % 3 == 0 && i % 10 != 0){
-            answer.push("짝")
-        }else if(Math.floor((i / 10) % 3) == 0 && Math.floor(i / 10) != 0){
-            answer.push("짝")
-        } else{
-            answer.push(i)
-        }
-    }
-    return answer
-}
+// function sol(input){
+//     let answer = []
+//     for(let i = 1; i <= input; i++){
+//         if((i % 10) % 3 == 0 && i % 10 != 0){
+//             answer.push("짝")
+//         }else if(Math.floor((i / 10) % 3) == 0 && Math.floor(i / 10) != 0){
+//             answer.push("짝")
+//         } else{
+//             answer.push(i)
+//         }
+//     }
+//     return answer
+// }
 
-console.log(sol(30))
+// console.log(sol(30))
+
+
+// 14번 - 숫자출력
+// function sol(input){
+//     let answer = []
+//     for(let i = 1; i <= input; i++){
+//         answer.push(i)
+//     }
+//     return answer
+// }
+
+// console.log(sol(5))
+
+
+// 15번 - 노동요
+//첫 시도
+// function sol(input){
+//     let answer = []
+//     if(input === ' '){
+//         answer.push("링디기디기\n")
+//     }else if(input === '.'){
+//         answer.push("딩딩딩\n")
+//     }else{
+//         answer.push("링딩동 ")
+//     }
+//     return answer
+// }
+//@@@설명 +=를 해야지 내가 썻던것에서 추가로 기능이 작동함
+// function sol(input){
+//     let answer = [] 
+//     for(let i = 0; i < input.length; i++){
+//         if(input[i] === ' '){
+//             answer +="링디기디기\n"
+//         }else if(input[i] === '.'){
+//             answer += "딩딩딩\n"
+//         }else{
+//             answer +="링딩동 "
+//         }
+//     }
+//     return answer
+// }
+
+// console.log(sol('나는 손영진 입니다......'))
+
