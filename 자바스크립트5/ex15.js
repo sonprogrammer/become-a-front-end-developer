2a04749e536f904a1c49025112724164299c10ed
손영진 <ods04138@gmail.com>
2023-07-20 20:02:53 +0900

    practice2

--- 자바스크립트5/ex15.js
+++ 자바스크립트5/ex15.js
@@ -1,11 +1,32 @@
 // 3의 배수에 박수치기
-function sol(input){
-    let answer = 0;
+// function sol(input){
+//     let num = Number(input)
 
-    if((input+1) % 3 == 0){
-        return "짝"
-    }else{
-        return input+1
-    }
-}
-console.log(sol(3))
+//     if((num+1) % 3 == 0){
+//         return "짝"
+//     }else{
+//         return input+1
+//     }
+// }
+// console.log(sol(11))
+
+// 
+// function solution(input) {
+//     let answer = []
+//     let count = 0;
+//     // answer = input.split(', ')
+//     for(let i = 0; i <= input.length; i++){
+//         if(input[i] === '커피'){
+//             count++
+//         }
+//     }
+//       return count;
+//     }
+
+// console.log(solution(['과자', '과자', '과자', '커피', '과자', '웩', '음료수', '음료수', '과자', '커피', '커피', '커피','웩']))
+
+
+
+
+
+// 주문내용 계산하기
