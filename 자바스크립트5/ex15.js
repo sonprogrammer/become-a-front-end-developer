53a8232f693370bb8a6d07dceebfad3af9352263
손영진 <ods04138@gmail.com>
2023-07-21 18:32:46 +0900

    practice

--- 자바스크립트5/ex15.js
+++ 자바스크립트5/ex15.js
@@ -30,16 +30,52 @@
 
 
 // 주문내용 계산하기
-function sol(){
-    let americano = 3500
-    let cafe_latte = 3800;
-    let milk_tea = 4200
+// function sol(){
+//     let americano = 3500
+//     let cafe_latte = 3800;
+//     let milk_tea = 4200
 
-    let price1 = americano * 2 + milk_tea
-    let price2 = (cafe_latte * 2 + milk_tea * 2) * 0.9
+//     let price1 = americano * 2 + milk_tea
+//     let price2 = (cafe_latte * 2 + milk_tea * 2) * 0.9
 
-    return price1 + price2
+//     return price1 + price2
 
+// }
+
+// console.log(sol())
+
+
+
+// 1부터 N까지 합의 제곱구하기
+// function sol(num){
+//     let answer
+//     let count = 0;
+//     for(let i = 1; i <= num; i++){
+//         count += i
+//     }
+//     answer = count * count
+//     return answer
+// }
+
+// console.log(sol(10))
+
+
+//1부터 N까지 합의 제곱구하고 각 제곱근의 합과 차를 구하기
+function sol(num){
+    let answer
+    let answer1
+    let count = 0
+    let count1 = 0;
+    for(let i = 1; i <= num; i++){
+        count += i
+    }
+    for(let j = 1; j <= num; j++){
+        count1 += j*j
+    }
+    answer = count * count  //1부터 num까지 합의 제곱한 값
+
+    answer1 = count1
+    return answer - answer1
 }
 
-console.log(sol())
+console.log(sol(10))
