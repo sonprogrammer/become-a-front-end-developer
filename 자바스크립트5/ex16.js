4ec7592d29b62fae6a74699a5b0e0ec28df3f00e
손영진 <ods04138@gmail.com>
2023-07-24 19:15:18 +0900

    practice

---
+++ 자바스크립트5/ex16.js
@@ -0,0 +1,21 @@
+// 마천루 문제
+// function sol(input){
+//     let result = ''
+//     for(let i = 1; i <= input; i++){    //i는 층수임
+//         for(let j = 0; j < (i <= 5 ? i : 5); j++){ //j는 별모양찍어줌
+//             result += "*"
+            
+//         }
+//         result += "\n"
+//     }
+//     return result
+// }
+
+// console.log(sol(7))
+
+
+
+//문자열 검사하기
+function sol(str){
+    
+}
