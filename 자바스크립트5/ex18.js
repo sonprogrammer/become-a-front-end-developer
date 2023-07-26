d64f1afee912670ded4b2a4baf780875492865bd
손영진 <ods04138@gmail.com>
2023-07-26 12:52:02 +0900

    practice

---
+++ 자바스크립트5/ex18.js
@@ -0,0 +1,36 @@
+// // n줄 입력받기
+// function sol(input){
+//     let answer = []
+//     answer = input.split('\n')
+
+//     return answer
+// }
+
+
+// 문자열 자르기
+// function sol(input){
+//     let answer = []
+//     a = input.split(' ')
+//     answer.push(typeof a)
+//     answer.push(a[0])
+
+//     return answer
+
+// }
+// console.log(sol('1'))
+
+
+// 용돈 압수
+// function sol(input){
+//     let answer = []
+//     let a = input.split('bd')
+//     return a 
+// }
+
+
+//함수 만들고 호출하기 
+// function sol(x, y){
+//     return (x + y);
+// }
+
+// console.log(sol(1, 2))
