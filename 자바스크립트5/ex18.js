512d91576b3b684e99f560af5e6c4ffdb8ee4020
손영진 <ods04138@gmail.com>
2023-07-26 13:05:07 +0900

    practice1

--- 자바스크립트5/ex18.js
+++ 자바스크립트5/ex18.js
@@ -34,3 +34,66 @@
 // }
 
 // console.log(sol(1, 2))
+
+
+// 조건에 맞는 암호
+// (a <= b && a == d && b> c && c < 6) || (a == b && a == c && a == d)
+// function sol(input){
+//     let answer;
+
+//     let num = input.split(' ')
+//     let a = num[0]
+//     let b = num[1]
+//     let c = num[2]
+//     let d = num[3]
+//     answer = (a <= b && a == d && b> c && c < 6) || (a == b && a == c && a == d)
+
+//     return answer
+// }
+
+// console.log(sol('2 2 2 9'))
+
+
+// 약수 찾기
+// function sol(input){
+//     let answer = []
+//     for(let i = 1; i<=input; i++){
+//         if(input % i === 0){
+//             answer.push(i)
+//         }
+//     }
+//     return answer
+// }
+
+// console.log(sol(489))
+
+
+// 3, 6, 9게임
+
+// function sol(input){
+//     let answer = []
+//     for(let i = 1; i <= input; i++){
+//         if((i % 10) % 3 ==0 && i % 10 != 0){
+//             answer.push("짝")
+//         }else if(Math.floor(i / 10) % 3 == 0 && Math.floor(i /10) != 0){
+//             answer.push("짝")
+//         }else{
+//             answer.push(i)
+//         }
+//     }
+//     return answer
+// }
+
+// console.log(sol(33))
+
+
+// 숫자 출력
+// function sol(input){
+//     let answer = []
+//     for(let i = 1; i <= input; i++){
+//         answer.push(i)
+//     }
+//     return answer
+// }
+
+// console.log(sol(10))
