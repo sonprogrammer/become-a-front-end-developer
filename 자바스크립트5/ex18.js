7ca50152c7f4cbb9d3d9812dce5eda41012bb497
손영진 <ods04138@gmail.com>
2023-07-26 15:23:13 +0900

    practice3

--- 자바스크립트5/ex18.js
+++ 자바스크립트5/ex18.js
@@ -174,3 +174,122 @@
 // }
 // console.log(sol('10 140'))
 
+// 배수 판별기 
+// function solution(num) {
+//     let answer = [];
+//     if(num % 2 == 0){
+//         answer += "2의 배수입니다.\n"
+//     } if(num % 3 == 0){
+//         answer += "3의 배수입니다.\n"
+//     } if(num % 5 == 0){
+//         answer += "5의 배수입니다.\n"
+//       //   answer.join('\n')
+//     } if(num % 7 == 0){
+//         answer += "7의 배수입니다.\n"
+//     }
+//    return answer
+//   }
+
+// function solution(num) {
+//     let answer = [];
+//     if (num % 2 == 0) {
+//         answer.push("2의 배수입니다.");
+//     }
+//     if (num % 3 == 0) {
+//         answer.push("3의 배수입니다.");
+//     }
+//     if (num % 5 == 0) {
+//         answer.push("5의 배수입니다.");
+//     }
+//     if (num % 7 == 0) {
+//         answer.push("7의 배수입니다.");
+//     }
+//     if (answer.length >= 2) {
+
+//         return answer.join("\n");
+//     } else {
+//         return answer;
+//     }
+// }
+
+// console.log(solution(10));
+
+
+// 가운데 숫자
+// function sol(input){
+//     let answer = ""
+//     let num = input.slice(" ")
+
+//     let middleIndex = Math.floor(num.length /2)
+//     answer = Number(num[middleIndex])
+//     return answer
+// }
+
+// console.log(sol())
+
+
+//3,6,9다음 숫자 말하기
+// function sol(input){
+//     let answer =""
+//     if((input+1) % 3 === 0){
+//             answer += "짝"
+            
+//         }else{
+//             answer += (input+1)
+            
+//         }
+    
+//     return answer
+// }
+// console.log(sol(4))
+
+
+// 도도새
+// function sol(input){
+//     let count = 0;
+//     for(let i = 0; i <= input.length; i++){
+//         if(input[i] === "웩"){
+//             count++
+//         }
+//     }
+//     return count
+// }
+// console.log(sol())
+
+
+// 제곱근과 합의 제곱 차구하기
+// function sol(input){
+//     let answer //합의 제곱
+//     let count = 0 //input까지 합
+//     let answer2 //제곱근
+//     let count1 = 0
+//     for(let i = 1; i <=input; i++){
+//         count += i
+//     }
+//     for(let j = 1; j <= input; j++){
+//         count1 += j*j
+//     }
+//     answer = count * count
+//     answer2 = count1
+
+//     return answer - answer2
+// }
+
+// console.log(sol(10))
+
+
+// 마천루
+// function sol(input){
+//     let result = ''
+//     for(let i = 1; i<=input; i++){
+//         for(let j = 0; j < (i <= 5 ? i : 5); j++){
+//             result += "*"
+//         }
+//         if(input != i){
+//         result += "\n"
+//         }
+//     }
+//     return result
+// }
+// console.log(sol(15))
+
